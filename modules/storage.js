9aa38d162bc654154872119ef8a740715721dade
Zsolt Szabo <zsagia1@gmail.com>
2020-11-29 10:38:08 +0100

    Storage.js created

---
+++ modules/storage.js
@@ -0,0 +1,37 @@
+var Storage = function() {
+    let history = null;
+
+    this.addHistoryItem = (actionTime, player, action, result) => {
+        const historyItem = new HistoryItem(actionTime, player, action, result);
+
+        history.item.push(historyItem);
+    };
+
+    this.finishGame = (endTime) => {
+        history.endTime = endTime;
+
+        const histories = this.read(HISTORIES) || [];
+
+        histories.push(history);
+
+        this.save(HISTORIES, histories);
+    };
+
+    this.getHistory = () => history;
+
+    this.read = (name) => {
+        const data = localStorage.getItem(name);
+
+        return data ? JSON.parse(data) : null;
+    };
+
+    this.save = (name, data) => {
+        localStorage.setItem(name, JSON.stringify(data));
+    };
+
+    this.startGame = (startTime, players, gameMode, gameLevel) => {
+        history = new History(startTime, players, gameMode, gameLevel);
+
+        console.log("Game started at ", new Date(startTime));
+    };
+};
