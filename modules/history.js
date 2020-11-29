31485554b4a021ce748c67c3a59282b89aabac36
Zsolt Szabo <zsagia1@gmail.com>
2020-11-29 09:16:49 +0100

    Add historyItem to the history.js

--- modules/history.js
+++ modules/history.js
@@ -1,9 +1,15 @@
-var History = function() {
-    this.endTime = null;
-    this.gameLevel = gameLevel;
-    this.gameMode = gameMode;
-    this.item = [];
-    this.players = players;
-    this.startTime = startTime;
+var History = function(startTime, players, gameMode, gameLevel) {
+    this.endTime    = null;
+    this.gameLevel  = gameLevel;
+    this.gameMode   = gameMode;
+    this.item       = [];
+    this.players    = players;
+    this.startTime  = startTime;
 };
 
+var HistoryItem = function(actionTime, player, action, result) {
+    this.action     = action;
+    this.actionTime = actionTime;
+    this.player     = player;
+    this.result     = result;
+};
