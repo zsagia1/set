3937ec5a806fd8d1704c15184cee35bcddef6cd6
Zsolt Szabo <zsagia1@gmail.com>
2020-11-29 09:13:34 +0100

    Create history.js

---
+++ modules/history.js
@@ -0,0 +1,9 @@
+var History = function() {
+    this.endTime = null;
+    this.gameLevel = gameLevel;
+    this.gameMode = gameMode;
+    this.item = [];
+    this.players = players;
+    this.startTime = startTime;
+};
+
